0907dad96e5c1413c8fd20674f31ffff88e06824
Elie Elisee Paul <marcoht023@hotmail.com>
2019-03-18 15:09:45 -0400

    added files keys and auth

---
+++ SuperMarketApp_Backend_starter/controllers/auth.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const passport = require('../middlewares/authentication');
+const redirect = require('../middlewares/redirect');
+
+const router = express.Router();
+router.get('/', 
+	passport.redirectIfLoggedIn('/'), 
+	(req, res) => {
+  		res.render('login', { error: req.flash('error')});
+});
+
+router.post('/', (req, res) => {
+   passport.authenticate('local', {
+      successRedirect: '/',
+      failureRedirect: '/login',
+      failureFlash: true,
+      successFlash: true,
+    })(req, res);
+});
+
+router.post('/jwt', passport.authenticate('jwt', {session: false}));
+
+router.get(
+		'/google', 
+		passport.authenticate('google', {
+			scope: ['profile', 'email']
+		})
+	);
+
+router.get('/google/callback',passport.authenticate('google', {
+	failureRedirect: '/login',
+	failureFlash: true,
+	successFlash: true,
+	}), (req, res) => {
+	res.redirect('/');
+	
+});
+
+module.exports = router;
