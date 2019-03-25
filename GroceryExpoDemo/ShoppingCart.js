9853cb38532e85b7cb8693cea936db4017217fc5
Ramon Petgrave <ramon.petgrave64@gmail.com>
2019-03-25 01:38:34 -0400

    added Cart Total at the bottom of the view

--- GroceryExpoDemo/ShoppingCart.js
+++ GroceryExpoDemo/ShoppingCart.js
@@ -12,24 +12,61 @@ const ShoppingCart = (props) => {
           fontSize: 18,
           height: 44,
           flexDirection: 'row',
+          borderRadius: 0,
+          borderWidth: 0.5,
+          borderColor: '#d6d7da',
           justifyContent: 'space-between',
         },
+        item_name: {
+            flexBasis: '50%',
+        },
+        item_quantity: {
+            textAlign: 'right',
+            flexBasis: '20%',
+        },
+        item_total: {
+            textAlign: 'right',
+            flexBasis: '30%',
+        },
+        cart_total: {
+            textAlign: 'right',
+            flexBasis: '30%',
+            flexGrow: 1,
+        }
     })
 
     return (
         <View style={styles.container}>
-          <FlatList
-            data={props.cart_data}
-            renderItem={
-              ({item}) =>
-                <View style={styles.item}>
-                  {/* <Image source={{uri: '#'}}/> */}
-                  <Text>{item.name}</Text>
-                  <Text>{item.price}</Text>
-                  <Text>{item.quantity}</Text>
-                </View>
-            }
-          />
+            {/* Cart Heading */}
+            <View style={styles.item}>
+                <Text style={styles.item_name}>Item Name</Text>
+                <Text style={styles.item_quantity}>Quantity</Text>
+                <Text style={styles.item_total}>Total Price</Text>
+            </View>
+            {/* Cart Contents */}
+            <FlatList
+                data={props.cart_data}
+                renderItem={
+                ({item}) =>
+                    <View style={styles.item}>
+                    {/* <Image source={{uri: '#'}}/> */}
+                    <Text style={styles.item_name}>{item.name}</Text>
+                    <Text style={styles.item_quantity}>{item.quantity}</Text>
+                    <Text style={styles.item_total}>{(item.price * item.quantity).toFixed(2)}</Text>
+                    </View>
+                }
+            />
+            {/* Cart Total */}
+            <View style={styles.item}>
+                <Text style={styles.cart_total}>Cart Total:</Text>
+                <Text style={styles.item_total}>
+                    {
+                        props.cart_data.reduce( (total, item) => {
+                            return total + item.price * item.quantity;
+                        }, 0 ).toFixed(2)
+                    }
+                </Text>
+            </View>
         </View>
     );
 }
