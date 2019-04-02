6f7ada195162c033089d20944bc2bf1cdcf0c325
Sebastianh77 <sebastianhenriquez9@gmail.com>
2019-04-02 19:07:21 -0400

    Added  '/products' path to index

--- SuperMarketApp_Backend_starter/controllers/index.js
+++ SuperMarketApp_Backend_starter/controllers/index.js
@@ -7,5 +7,6 @@ router.use('/', require('./home'));
 router.use('/login', require('./login'));
 router.use('/signup', require('./signup'));
 router.use('/payments', require('./payments'));
+router.use('/products', require('./products'));
 
 module.exports = router;
